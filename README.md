0654957e68f896320a37dbc8709b85f1dc928d33
zkonge <me@konge.pw>
2016-10-03 20:29:58 +0800

    New features

--- README.md
+++ README.md
@@ -1,10 +1,29 @@
 # Challenge
 **简单的多题库答题系统**
 
-[![PHP VERSION](https://img.shields.io/badge/PHP-5.4+-orange.svg?maxAge=2592000)]()
+[![PHP VERSION](https://img.shields.io/badge/PHP-5.4+-green.svg?maxAge=2592000)]()
 
 >需要
 
 > 1. PHP5.4+
 
 > 2. mcrypt
+
+##食用方法
+
+1.修改 $rawKey (可以滚键盘), 每次修改需同时修改 $version 使前端自动更新key
+
+2.添加文件到 /questions 文件夹, 文件名为 题库名+'.php' (不包含双引号).
+
+格式:
+```php
+<?php
+$question=[
+  ['题目0','答案0'],
+  ['题目1','答案1'],
+  //...
+  ['答完了',[1]] //防止越界
+];
+```
+
+3.愉快地玩耍
--- ajax.php
+++ ajax.php
@@ -3,11 +3,11 @@ if(!isset($_POST['mod']))exit;
 
 error_reporting(0);
 
-include 'questions.php';
-
-$key = 'haha';
-$key = $_SERVER['HTTP_USER_AGENT'].$key.$_SERVER["REMOTE_ADDR"];
+$rawKey = 'haha';
 $version = 0;
+
+$key = md5($_SERVER['HTTP_USER_AGENT'].$rawKey.$_SERVER["REMOTE_ADDR"]);
+$questionFolderPath = 'question/';
 
 function encrypt($encrypt, $key){
   $iv = mcrypt_create_iv ( mcrypt_get_iv_size ( MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB ), MCRYPT_RAND );
@@ -28,46 +28,47 @@ $ret = [
   'status' => 0
 ];
 
+$questionPool = $_POST['questionPool'];
+
+if(isset($_POST['key'])){
+  $info = json_decode(decrypt($_POST['key'], $key), true);
+
+  $questionPool = $info[0];
+  $maxProgress = $info[1];
+  $progress = (int)$_POST['progress'];
+}
+
+$questionPath = $questionFolderPath.$questionPool.'.php';
+if(!isset($questionPool) || !file_exists($questionPath)){
+  $questionPool = 'default';
+  $questionPath = $questionFolderPath.'default.php';
+}
+if($_POST['mod']!='getKey')include $questionPath;
+
 switch($_POST['mod']){
   case 'getHint':
-    $info = json_decode(decrypt($_POST['key'], $key), true);
 
-    if($info[2]<$version){
-      $ret=['status'=>2];
+    if($info[2]!=$version){
+      $ret = ['status'=>2];
       break;
     }
-
-    $questionPool = $info[0];
-    $maxProgress = $info[1];
-    $progress = (int)$_POST['progress'];
 
     if($maxProgress<$progress)break;
 
     $ret = [
       'status' => 1,
-      'hint' => $questions[$questionPool][$progress][0]
+      'hint' => $questions[$progress][0]
     ];
     break;
 
   case 'checkAnswer':
-    //It won't slow down a lot
-    $info = json_decode(decrypt($_POST['key'], $key), true);
-
-    if($info[2]<$version){
-      $ret=['status'=>2];
-      break;
-    }
-
-    $questionPool = $info[0];
-    $maxProgress = $info[1];
-    $progress = (int)$_POST['progress'];
     $answer = $_POST['answer'];
 
-    if(!($progress>=0 && $progress<count($questions[$questionPool])))break;
+    if(!($progress>=0 && $progress<count($questions)))break;
 
     if($maxProgress<$progress)break;
 
-    if($questions[$questionPool][$progress][1]===$answer)
+    if($questions[$progress][1]===$answer)
       if($maxProgress===$progress)
         $ret = [
           'status' => 1,
@@ -81,11 +82,6 @@ switch($_POST['mod']){
     break;
 
   case 'getKey':
-    if(isset($_POST['questionPool'], $questions[$_POST['questionPool']]))
-      $questionPool = $_POST['questionPool'];
-    else
-      $questionPool = 'default';
-
     $ret = [
       'status' => 1,
       'key' => encrypt(json_encode([$questionPool, 0, $version]), $key)
